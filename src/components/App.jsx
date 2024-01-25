0247a0cee3fee9166fb09ac57b317e88f2809aad
Veregorn <rjbarco@gmail.com>
2024-01-25 06:38:58 +0100

    Now 'App.jsx' load all his children Components and their names are painted

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,19 @@
 import '../styles/App.css'
+import Header from './Header'
+import Playground from './Playground'
+import SelectPanel from './SelectPanel'
+import Footer from './Footer'
+import Video from './Video'
 
 function App() {
 
   return (
     <>
-
+      <Header />
+      <Playground />
+      <SelectPanel />
+      <Footer />
+      <Video />
     </>
   )
 }
